7f685672a759515438e40e3ed2c344fb9086c6fb
Zaphryx1 <95585542+Zaphryx1@users.noreply.github.com>
2022-03-06 23:54:15 -0600

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// remove splash screen
+
+// $("start") onclick(function() {
+//     $("splash").remove();
+// });
+
+// timer
+
+// questions
+
+// evaluate if answer was right or wrong
+
+// add to leaderboard
